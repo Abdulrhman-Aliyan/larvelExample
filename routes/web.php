65b0ea35e4647d8d838a2ae6787a176159a0b769
Abdulrhman Aliyan <68137382+Abdulrhman-Aliyan@users.noreply.github.com>
2025-01-01 03:06:36 +0300

    test routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+/* Route::get('/store', function () {
+    $filter = request('style');
+
+    if(isset($filter)) {
+        return '<h1> this page is viewing <span style="color: red">'. strip_tags($filter) .'</span> </h1>';
+    }
+
+    return '<h1> this page is viewing <span style="color: red">All Products</span> </h1>';
+});
+ */
+
+ Route::get('/store/{category?}/{item?}', function ($category = null, $item = null) {
+    if(isset($category)) {
+        if(isset($item)) {
+            return "<h1>{$item}</h1>";
+        }
+        return "<h1>{$category}</h1>";
+    }
+
+    return '<h1>Store</h1>';
+});
